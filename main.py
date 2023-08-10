3c77efcfa687f0d08f1f04f7fbe5cea15764b2f6
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-10 12:42:56 +0200

    save uploaded file

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, url_for
+import os
 from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import SubmitField, FileField
 import secrets
 
 
-UPLOAD_FOLDER = r"C:\Users\zbign_x5x2ftd\PycharmProjects\DetectingColor\uploads"
+UPLOAD_FOLDER = ".\static"
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
 app = Flask(__name__)
@@ -21,18 +23,17 @@ bootstrap = Bootstrap5(app)
 
 
 class UploadImageForm(FlaskForm):
-    image = FileField('Image')
+    image = FileField('Image', validators=[InputRequired()])
     submit = SubmitField("Submit File")
-
-def allowed_file(image):
-    return "." in image and image.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/", methods=("GET", "POST"))
 def home():
     form = UploadImageForm()
     if form.validate_on_submit():
-        file = form.image
+        file = form.image.data
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+        return "File updated correctly"
     return render_template("home.html", form=form)
 
 
